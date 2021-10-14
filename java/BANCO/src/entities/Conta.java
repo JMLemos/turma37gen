f73a901ff5cdd434730c03768e30429f48e1e9bb
JMLemos <jlemosdecastro@gmail.com>
2021-10-13 22:39:32 -0300

    wip

--- java/BANCO/src/entities/Conta.java
+++ java/BANCO/src/entities/Conta.java
@@ -1,5 +1,58 @@
 package entities;
 
-public class Conta {
+public abstract class Conta {
+	
+	//atributos
+	private int numero;
+	private String cpf;
+	private double saldo;
+	private boolean ativo;
+	
+	//construtor
+	public Conta(int numero, String cpf) {
+		super();
+		this.numero = numero;
+		this.cpf = cpf;
+	}
+	
+	//encapsulamento
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public double getSaldo() {
+		return saldo;
+	}
+	public boolean isAtivo() {
+		return ativo;
+	}
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
+	
+	//metodos
+	
+	public void ativarConta () {
+		boolean ativarconta = true;
+	}
+	
+	public double debito (double valor) {
+		return saldo -= valor;
+	}
+	
+	public double credito (double valor) {
+		return saldo += valor;
+	}
+
+	
+	
 
 }
--- java/BANCO/src/entities/Poupanca.java
+++ java/BANCO/src/entities/Poupanca.java
@@ -1,5 +1,34 @@
 package entities;
 
-public class Poupanca {
+public class Poupanca extends Conta {
+	
+	//atributos
+	private int diaAniversarioPoupanca;
+	
+	//construtor
+	public Poupanca(int numero, String cpf, int diaAniversarioPoupanca) {
+		super(numero, cpf);
+		this.diaAniversarioPoupanca = diaAniversarioPoupanca;
+	}
+	//encapsulamento
+	public int getDiaAniversarioPoupanca() {
+		return diaAniversarioPoupanca;
+	}
+
+	public void setDiaAniversarioPoupanca(int diaAniversarioPoupanca) {
+		this.diaAniversarioPoupanca = diaAniversarioPoupanca;
+	}
+	
+	//metodo
+	
+	public double correcao() {
+		
+		return (super.getSaldo()*0.0005 + super.getSaldo());
+		
+	}
+	
+	
+	
+	
 
 }
