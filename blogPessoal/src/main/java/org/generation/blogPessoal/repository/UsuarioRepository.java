b4708183fe738baf5bd5e17b60ff941e6d77f2bc
JMLemos <jlemosdecastro@gmail.com>
2021-11-19 09:50:32 -0300

    @JMLemos [Swagger e configuracoes de security]

--- blogPessoal/src/main/java/org/generation/blogPessoal/repository/UsuarioRepository.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/repository/UsuarioRepository.java
@@ -5,8 +5,9 @@ import java.util.Optional;
 import org.generation.blogPessoal.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-	public Optional <Usuario> findByUsuario(String usuario);
+	public Optional <Usuario> findByUsuario(String nomeCompleto);
 	
 
 }
