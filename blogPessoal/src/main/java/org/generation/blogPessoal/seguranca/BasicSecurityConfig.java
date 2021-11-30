b324b5ac8268b3bfb66d6bc566a49d4680c26947
JMLemos <jlemosdecastro@gmail.com>
2021-11-29 21:14:55 -0300

    ajustes solicitados pelo PO no blogPessoal

--- blogPessoal/src/main/java/org/generation/blogPessoal/seguranca/BasicSecurityConfig.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/seguranca/BasicSecurityConfig.java
@@ -3,6 +3,7 @@ package org.generation.blogPessoal.seguranca;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -33,17 +34,20 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 		return new BCryptPasswordEncoder();
 	}
 	
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/usuarios/logar").permitAll()
+		http.authorizeRequests()
+		.antMatchers("/usuarios/logar").permitAll()
 		.antMatchers("/usuarios/cadastrar").permitAll()
+		.antMatchers(HttpMethod.OPTIONS).permitAll()
 		.anyRequest().authenticated()
 		.and().httpBasic()
 		.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 		.and().cors()
 		.and().csrf().disable();
 		
-		
 	}
+	
 
 }
